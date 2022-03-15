dbdb28aa338f3c7c102420f82f0c34e72b8a017a
sergiorodas <sergiorodasgonzalez@gmail.com>
2022-03-15 15:48:42 -0300

    Express

--- Section7/Class-3/index.js
+++ Section7/Class-3/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const server = express();
 
+const fs = require('fs');
+
+const home = fs.readFileSync('./index.html');
+const about = fs.readFileSync('./about.html');
+
+server.get('/', (req, res) => {
+    res.write(home);
+})
+
+server.get('/about', (req, res) => {
+    res.write(about);
+})
+
 server.listen(8080, () => {
     console.log('Server is running on port ' + 8080);
 })
